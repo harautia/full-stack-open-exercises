7e262c83ef0f8683e260ec6c436d10e4d0519ea8
Hannu Rautiainen <harautia1976@gmail.com>
2025-01-06 16:08:21 +0200

    Create step2-app.jsx

---
+++ part1/unicafe/step2-app.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(updatedGood + neutral + bad)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(good + updatedNeutral + bad)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(good + neutral + updatedBad)
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={handleGoodClick}> good </button>
+      <button onClick={handleNeutralClick}> neutral </button>
+      <button onClick={handleBadClick}> bad </button>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good-bad) / all}</p>
+      <p>positive {good/all * 100} %</p>
+    </div>
+  )
+}
+
+export default App
