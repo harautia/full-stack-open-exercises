73cf11a17472532becbdcbd697bc484e3f8770e3
Hannu Rautiainen <harautia1976@gmail.com>
2025-01-20 17:44:30 +0200

    Update step6-Content.jsx

--- part2/courseinfo/step6-Content.jsx
+++ part2/courseinfo/step6-Content.jsx
@@ -4,10 +4,10 @@ const Content = (props) => {
     console.log(props)
     return (
       <div>
-        <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises} />
-        <Part part={props.course.parts[1].name} exercise={props.course.parts[1].exercises} />
-        <Part part={props.course.parts[2].name} exercise={props.course.parts[2].exercises} />
+        {props.course.parts.map((part, index) => (
+            <Part key={index} part={part.name} exercise={part.exercises} />
+          ))
+        }
       </div>
-    )
-  }
+    )}
 export default Content
