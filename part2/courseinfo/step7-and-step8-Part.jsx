03e032b601f9f551e4c18cef1d9b104aa3af6991
Hannu Rautiainen <harautia1976@gmail.com>
2025-01-20 18:07:50 +0200

    Create step7-and-step8-Part.jsx

---
+++ part2/courseinfo/step7-and-step8-Part.jsx
@@ -0,0 +1,12 @@
+const Part = (props) => {
+    console.log(props)
+    return (
+      <div>
+        <p>
+          {props.part} {props.exercise}
+        </p>
+      </div>
+    )
+  }
+  
+export default Part
