fd81176464cecc9b243ef1d23619ccb5a9d52b9f
Hannu Rautiainen <harautia1976@gmail.com>
2025-02-17 18:39:33 +0200

    Create step10-notes.js

---
+++ part2/phonebook/step10-notes.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getData = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+  }
+
+const addData = newObject => {
+  console.log()
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const updateData = (existingId, modifiedData) => {
+  console.log(existingId, modifiedData)
+  const request = axios.put(`${baseUrl}/${existingId}`, modifiedData)
+  return request.then(response => response.data)
+}
+
+const deleteData = userId => {
+  console.log()
+  const request = axios.delete(`${baseUrl}/${userId}`)
+  return request.then(response => response.data)
+}
+
+export default { getData, addData, deleteData, updateData }
