49bff2b20be77bb7ed8988b5cd5a31d378b110c2
Hannu Rautiainen <harautia1976@gmail.com>
2025-02-22 14:34:00 +0200

    Create step11-App.jsx

---
+++ part2/phonebook/step11-App.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+import noteService from './services/notes'
+import Filter from './components/Filter'
+import AddData from './components/AddData'
+import Persons from './components/Persons'
+import Notification from './components/Alerts'
+
+
+const App = () => {
+
+  const [persons, setPersons] = useState([])
+  useEffect(() => {
+    console.log('effect')
+    noteService
+    .getData()
+    .then(initialData => {
+      setPersons(initialData)
+    })
+  }, [])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchFilter, setFilter] = useState('')
+  const [notificatioMessage, setNotificationMessage] = useState(null)
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+  
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const handleAddData = (event) => {
+    event.preventDefault();
+    AddData({ persons, setPersons, name: newName, number: newNumber });
+    setNewName('');
+    setNewNumber('');
+    setNotificationMessage(
+      `Added '${newName}'`
+    )
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 5000)
+  };
+
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <Notification message={notificatioMessage} />
+        <div>Find Users: <input value={searchFilter} onChange={handleFilter}/></div>
+        <Filter persons={persons} filter={searchFilter} />
+        <h3>Add a new</h3>
+        <form onSubmit={handleAddData}>
+          <div>Name: <input value={newName} onChange={handleNameChange}/></div>
+          <div>Number: <input value={newNumber} onChange={handleNumberChange} /></div>
+          <div><button type="submit">add</button></div>
+        </form>
+        <h3>Numbers</h3>
+        <Persons persons={persons} setPersons={setPersons}/>
+      </div>
+    )
+}
+
+export default App
